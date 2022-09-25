df8c63c55e6251218fa0809842df7e09452c0766
lokerd <lokerd5110@yandex.ru>
2022-09-25 19:09:48 +0300

    done

---
+++ GraphicsLab1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace GraphicsLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectedFigure = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.figureForm = new System.Windows.Forms.ListBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.blinking = new System.Windows.Forms.Button();
+            this.floating_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedFigure)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectedFigure
+            // 
+            this.selectedFigure.BackColor = System.Drawing.Color.White;
+            this.selectedFigure.Location = new System.Drawing.Point(225, 0);
+            this.selectedFigure.Name = "selectedFigure";
+            this.selectedFigure.Size = new System.Drawing.Size(675, 500);
+            this.selectedFigure.TabIndex = 0;
+            this.selectedFigure.TabStop = false;
+            this.selectedFigure.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // figureForm
+            // 
+            this.figureForm.FormattingEnabled = true;
+            this.figureForm.Items.AddRange(new object[] {
+            "Traiangle",
+            "Square",
+            "Circle"});
+            this.figureForm.Location = new System.Drawing.Point(16, 21);
+            this.figureForm.Name = "figureForm";
+            this.figureForm.Size = new System.Drawing.Size(192, 43);
+            this.figureForm.TabIndex = 2;
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(16, 70);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(192, 59);
+            this.drawButton.TabIndex = 10;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // blinking
+            // 
+            this.blinking.Location = new System.Drawing.Point(16, 135);
+            this.blinking.Name = "blinking";
+            this.blinking.Size = new System.Drawing.Size(192, 59);
+            this.blinking.TabIndex = 11;
+            this.blinking.Text = "Blinking";
+            this.blinking.UseVisualStyleBackColor = true;
+            this.blinking.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // floating_button
+            // 
+            this.floating_button.Location = new System.Drawing.Point(16, 200);
+            this.floating_button.Name = "floating_button";
+            this.floating_button.Size = new System.Drawing.Size(192, 53);
+            this.floating_button.TabIndex = 12;
+            this.floating_button.Text = "Floating";
+            this.floating_button.UseVisualStyleBackColor = true;
+            this.floating_button.Click += new System.EventHandler(this.floating_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 500);
+            this.Controls.Add(this.floating_button);
+            this.Controls.Add(this.blinking);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.figureForm);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.selectedFigure);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.selectedFigure)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox selectedFigure;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox figureForm;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.Button blinking;
+        private System.Windows.Forms.Button floating_button;
+    }
+}
+
---
+++ GraphicsLab1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsLab1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new System.Drawing.Size(900, 500);
+        }
+
+        Graphics g;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = selectedFigure.CreateGraphics();
+        }
+
+
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            blink = false;
+            floating = false;
+            figures.Clear();
+            SolidBrush back = new SolidBrush(Color.White);
+            g.FillRectangle(back, 0, 0, 675, 500);
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int figure = figureForm.SelectedIndex;
+            int xPos = rnd.Next(0, 475), yPos = rnd.Next(0, 300),  size = rnd.Next(50, 200);
+            int colorR = rnd.Next(0, 255), colorG = rnd.Next(0, 255), colorB = rnd.Next(0, 255);
+
+            draw_figure(figure, colorR, colorG, colorB, xPos, 0, size);
+        }
+
+        private void draw_figure(int figure, int colorR, int colorG, int colorB,  int xPos, int yPos, int size)
+        {
+
+            SolidBrush randColor = new SolidBrush(Color.FromArgb(colorR, colorG, colorB));
+            
+            switch (figure)
+            {
+                case 0:
+                    g.FillPolygon(randColor, new Point[]
+                        {
+                            new Point(xPos + size/2, yPos),new Point(xPos, yPos + size),
+                            new Point(xPos, yPos + size),new Point(xPos + size, yPos + size),
+                            new Point(xPos + size, yPos + size),new Point(xPos + size/2, yPos)
+                        });
+                    break;
+                case 1:
+                    g.FillRectangle(randColor, xPos, yPos, size, size);
+                    break;
+                case 2:
+                    g.FillEllipse(randColor, xPos, yPos, size, size);
+                    break;
+                case 3:
+                    break;
+            }
+        }
+
+        private void draw_border(int figure, int colorR, int colorG, int colorB, int xPos, int yPos, int size, int width)
+        {
+            Pen randColor = new Pen(Color.FromArgb(colorR, colorG, colorB), width);
+            switch (figure)
+            {
+                case 0:
+                    g.DrawPolygon(randColor, new Point[]
+                        {
+                            new Point(xPos + size/2, yPos),new Point(xPos, yPos + size),
+                            new Point(xPos, yPos + size),new Point(xPos + size, yPos + size),
+                            new Point(xPos + size, yPos + size),new Point(xPos + size/2, yPos)
+                        });
+                    break;
+                case 1:
+                    g.DrawRectangle(randColor, xPos, yPos, size, size);
+                    break;
+                case 2:
+                    g.DrawEllipse(randColor, xPos, yPos, size, size);
+                    break;
+                case 3:
+                    break;
+            }
+        }
+
+        static Mutex mutex = new Mutex();
+        bool blink = false;
+        async private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int size = rnd.Next(50, 200);
+            int xPos = rnd.Next(0, 475), yPos = rnd.Next(0, 300);
+
+            int figure = figureForm.SelectedIndex;
+
+            int colorR = rnd.Next(0, 255), colorG = rnd.Next(0, 255), colorB = rnd.Next(0, 255);
+            int width = rnd.Next(2, 10);
+
+            blink = true;
+
+            void blinkBorder(int time)
+            {
+                int borderColorR = colorR, borderColorG = colorG, borderColorB = colorB;
+                while (blink)
+                {
+                    mutex.WaitOne();
+                        draw_figure(figure, 255, 255, 255, xPos, yPos, size);
+                        draw_border(figure, 255, 255, 255, xPos, yPos, size, width);
+                        draw_figure(figure, colorR, colorG, colorB, xPos, yPos, size);
+                        draw_border(figure, borderColorR, borderColorG, borderColorB, xPos, yPos, size, width);
+                    mutex.ReleaseMutex();
+
+                    borderColorR = rnd.Next(0, 255);
+                    borderColorG = rnd.Next(0, 255);
+                    borderColorB = rnd.Next(0, 255);
+                    Thread.Sleep(time);
+                }
+                
+            }
+
+            int rndTime = rnd.Next(200, 1000);
+            async Task DrawAsync()
+            {
+                Console.WriteLine("Начало метода blinkBorder");
+                await Task.Run(() => blinkBorder(rndTime));
+                Console.WriteLine("Конец метода blinkBorder");
+            }
+
+            await DrawAsync();
+           
+        }
+
+
+        List<int> figures = new List<int>();
+        bool floating = false;
+        async private void floating_button_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int figure = figureForm.SelectedIndex;
+            int xPos = rnd.Next(5, 475), yPos = rnd.Next(5, 300), size = rnd.Next(50, 200);
+            bool pos = true;
+            while(pos)
+            {
+                int c = 0;
+                for(int i = 0; i < figures.Count; i += 5)
+                {
+                    if (xPos + size >= figures[i] && xPos <= figures[i] + figures[i + 2]) c++;
+                    if (yPos + size >= figures[i + 1] && yPos <= figures[i + 1] + figures[i + 2]) c++;
+                }
+                if (c==0) { pos = false; } else { xPos = rnd.Next(5, 475); yPos = rnd.Next(5, 300); }
+            }
+            int colorR = rnd.Next(0, 255), colorG = rnd.Next(0, 255), colorB = rnd.Next(0, 255);
+            int switchR = rnd.Next(1, 20), switchG = rnd.Next(1, 20), switchB = rnd.Next(1, 20);
+            int figId = figures.Count;
+            figures.Add(xPos);
+            figures.Add(yPos);
+            figures.Add(size);
+            figures.Add(1);
+            figures.Add(1);
+
+            floating = true;
+
+            void floatFigure()
+            {
+                while (floating)
+                {
+                    mutex.WaitOne();
+                    {
+                        draw_figure(figure, 255, 255, 255, xPos, yPos, size);
+
+                        if (xPos <= 0) figures[figId + 3] *= -1;
+                        if (xPos + size >= 675) figures[figId + 3] *= -1;
+                        if (yPos <= 0) figures[figId + 4] *= -1;
+                        if (yPos + size >= 500) figures[figId + 4] *= -1;
+
+                        for(int i = 0; i < figures.Count; i += 5)
+                        {
+                            if(i != figId)
+                            {
+                                int x2Pos = figures[i], y2Pos = figures[i + 1], size2 = figures[i + 2];
+                                if (xPos + size >= x2Pos && xPos <= x2Pos + size)
+                                {
+                                    if (yPos + size + 1 * figures[figId + 4] == y2Pos || yPos + 1 * figures[figId + 4] == y2Pos + size2) { figures[figId + 4] *= -1; figures[i + 4] *= -1; }
+                                }
+                                if (yPos + size >= y2Pos && yPos <= y2Pos + size)
+                                {
+                                    if (xPos + size + 1 * figures[figId + 3] == x2Pos || xPos + 1 * figures[figId + 3] == x2Pos + size2) {figures[figId + 3] *= -1; figures[i + 3] *= -1; }
+                                }
+                            }
+                            
+                        }
+
+                        xPos += 1 * figures[figId + 3];
+                        yPos += 1 * figures[figId + 4];
+
+                        figures[figId] = xPos;
+                        figures[figId + 1] = yPos;
+
+                        if (colorR + switchR > 255) switchR *= -1;
+                        if (colorR + switchR < 0) switchR *= -1;
+                        if (colorG + switchG > 255) switchG *= -1;
+                        if (colorG + switchG < 0) switchG *= -1;
+                        if (colorB + switchB > 255) switchB *= -1;
+                        if (colorB + switchB < 0) switchB *= -1;
+
+                        colorR += switchR;
+                        colorG += switchG;
+                        colorB += switchB;
+
+                        draw_figure(figure, colorR, colorG, colorB, xPos, yPos, size);
+                    }
+                    mutex.ReleaseMutex();
+                    Thread.Sleep(50);
+                }
+
+            }
+
+            async Task DrawAsync()
+            {
+                Console.WriteLine("Начало метода blinkBorder");
+                await Task.Run(() => floatFigure());
+                Console.WriteLine("Конец метода blinkBorder");
+            }
+
+            await DrawAsync();
+        }
+    }
+}
